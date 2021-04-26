bdb8a2433a68d256fd02798b71107330e85e65ba
rakes <rakes@LAPTOP-6L6AHHU2>
2021-04-26 15:26:57 +0530

    Initial commit.

---
+++ src/JavaClass1.java
@@ -0,0 +1,10 @@
+
+public class JavaClass1 {
+	public static void main(String[] args) {
+		int a = 5;
+		if (a % 2 == 0)
+			System.out.println("Odd number");
+		else
+			System.out.println("Even number");
+	}
+}
